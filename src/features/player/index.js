f2f7af78ec4bd752175cd814cde41f9ea77bcc5e
Jasmin <jasminf020@gmail.com>
2020-01-06 21:27:00 +0200

    Start working on my first game :}

---
+++ src/features/player/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {connect} from'react-redux'
+import playerWalk from './player_walk.png'
+
+function player(props) {
+    return(
+      <div style={{
+          position: 'relative',
+          top: props.position[1],
+          left: props.position[0],
+          backgroundImage: `url('${playerWalk}')`,
+          backgroundPosition: '0 0',
+          width: '40px',
+          height: '40px',
+      }}> </div>
+    )
+}
+
+function mapStateToProps(state) {
+    return{
+      ...state.player,
+    }
+}
+
+export default connect(mapStateToProps) (player);
